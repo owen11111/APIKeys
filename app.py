c3513df6cd510e6cfa7105d9b819891f7609bba2
owen11111 <47312543+owen11111@users.noreply.github.com>
2024-07-31 13:36:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ validAPIKeys = ['1234', '5678']
 def apiForm():
     key = request.headers.get('APIkey')
     if key in validAPIKeys:
-        return Response({
-                "Question 1": "Example Response",
-                "Question 2": "Example Response",
-                "Question 3": "Example Response",
-                "Question 4": "Example Response",
-                "Question 5": "Example Response"
-                }, status=200)
+        response_data = {
+            "Question 1": "Example Response",
+            "Question 2": "Example Response",
+            "Question 3": "Example Response",
+            "Question 4": "Example Response",
+            "Question 5": "Example Response"
+        }
+        return jsonify(response_data), 200
     else:
         return Response("Invalid Credentials", status=401)
 
